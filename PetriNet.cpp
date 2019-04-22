589a04d1024dfe6599c18b5b6c0f1640c2a9370d
Aleksey Perkov <niktesla96@gmail.com>
2019-04-23 01:40:23 +0300

    Check

--- PetriNet.cpp
+++ PetriNet.cpp
@@ -5,6 +5,8 @@
 PetriNet::PetriNet()
 {
 	//init
+	net[0][0] = 0; M[0] = 0;
+	sizeP = 1; sizeT = 1;
 }
 
 
@@ -38,52 +40,103 @@ short int PetriNet::findT(short int p1, short int p2) //ищем переход
 
 void PetriNet::jump(short int T) //используем переход
 {
-	bool marker=0;
-	for (int p = 0; p < M.size(); p++) {  //доступные позиции
-		if (net[p][T] != 0) {
-			if (net[p][T] < 0 && M[p]>0) {
-				M[p]--; marker = true;
-			}
-			if (net[p][T] > 0 && marker==1) {
-				M[p]++; 
-			}
-
+	bool marker = false;
+	for (int p = 0; p < M.size(); p++)
+	{  //доступные позиции
+		if (net[p][T] < 0 && M[p]>0)
+		{
+			M[p]--; marker = true;
+		}
+	}
+	if(marker)
+	for (int p = 0; p < M.size(); p++)
+	{
+		if (net[p][T] > 0)
+		{
+			M[p]++; 
 		}
 	}
 }
 
-vector<short int> PetriNet::algorithm(short int pBegin, short int pEnd) //путь из p1 в p2
+
+void PetriNet::algorithm(short int pBegin, short int pEnd) //путь из p1 в p2
 {
-	vector<short int>  T,P;
-	short int p1 = pBegin, p2; 
+	vector<short int>  T, P;
+	short int p1 = pBegin, p2;
 	bool b = false;
 	for (int p = 0; p < M.size(); p++)
 	{
 		p2 = p;
 		b = transPossibility(p1, p2);
-		/*if (b && p2 = pEnd) 
+		if (p2 == pEnd && b)
 		{
 			T.push_back(findT(p1, p2)); //по какому t
 			for (int i = 0; i < T.size(); i++)
 			{
 				jump(T[i]);
-			} 
-			break ; //по ним делаем jump
-		}*/
+			}
+			break; //по ним делаем jump
+		}
 		if (b)
 		{
 			T.push_back(findT(p1, p2));
 			P.push_back(p1); //откуда
-			p1 = p2;	
+			p1 = p2;
 			p = -1;
 		}
 		else
 		{
 			p = p1;
 			T.pop_back(); P.pop_back();
-			p1 = P[P.size()-1];
+			p1 = P[P.size() - 1];
 		}
 	}
 	//добавить вывод что, переход не возможен
-	return T;
+}
+void PetriNet::addP(short int p)
+{
+	const int P = p;
+	if (M.count(P) == 0)
+	{
+		M[p] = 0;
+		for (int i = 0; i < net[0].size(); i++) {
+			net[p][i] = 0;
+			sizeP++;
+		}
+	}
+}
+
+void PetriNet::addT(short int t)
+{
+	const int T = t;
+	if (net[0].count(T) == 0)
+		for (int i = 0; i < M.size(); i++) {
+			net[i][t] = 0;
+			sizeT++;
+		}
+}
+
+
+void PetriNet::addD(short int p, short int t, short int d)
+{
+	const int P = p;
+	const int T = t;
+	if (M.count(P) > 0)
+		if (net[0].count(T) > 0)
+			net[p][t] = d;
+}
+
+void PetriNet::addM(short int p, short int val)
+{
+	const int P = p;
+	if (M.count(P) > 0) M[p] = val;
+}
+
+short int PetriNet::atD(short int p, short int t)
+{
+	return net[p][t];
+}
+short int PetriNet::atM(short int p)
+{
+	return M[p];
 }
--- PetriNet.h
+++ PetriNet.h
@@ -4,16 +4,23 @@
 using namespace std;
 class PetriNet
 {
-	short int sizeP, sizeT;
+	
 	unordered_map<short int, unordered_map<short int, short int>> net; //PxT матрица со значениями -1,0,1
-	vector<short int> M; //матрица маркеров
+	unordered_map<short int, short int> M; //матрица маркеров
 public:
+	short int sizeP, sizeT;
 	PetriNet();
 	virtual ~PetriNet();
 	//bool correctNet();
 	short int findT(short int p1, short int p2);
 	bool transPossibility(short int p1, short int p2);
 	void jump(short int t);
-	vector<short int>  algorithm(short int p1, short int p2); //достижимость
+	void algorithm(short int pBegin, short int pEnd);
+	void addD(short int p, short int t, short int d);
+	void addM(short int p, short int val);
+	void addP(short int p);
+	void addT(short int t);
+	short int atD(short int p, short int t);
+	short int atM(short int p);
 };
 
